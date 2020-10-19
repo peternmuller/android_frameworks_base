2e8ddc17cfa0e07a5f27c73ca789f58e3f48d818
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-10-19 17:23:14 +0000

    SystemUI: Screenshot: Add delete action chip intent
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: Ia55ac91c2db2f44062674a699f8df63c96e1de5c

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -140,6 +140,7 @@ public class ScreenshotController implements ScreenshotHandler {
      */
     public static class SavedImageData {
         public Uri uri;
+        public Notification.Action deleteAction;
         public List<Notification.Action> smartActions;
         public Notification.Action quickShareAction;
         public UserHandle owner;
@@ -151,6 +152,7 @@ public class ScreenshotController implements ScreenshotHandler {
          */
         public void reset() {
             uri = null;
+            deleteAction = null;
             smartActions = null;
             quickShareAction = null;
             subject = null;
@@ -198,6 +200,7 @@ public class ScreenshotController implements ScreenshotHandler {
     public static final String EXTRA_ACTION_INTENT_FILLIN =
             "android:screenshot_action_intent_fillin";
 
+    static final String SCREENSHOT_URI_ID = "android:screenshot_uri_id";
 
     // From WizardManagerHelper.java
     private static final String SETTINGS_SECURE_USER_SETUP_COMPLETE = "user_setup_complete";
