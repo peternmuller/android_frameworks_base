ca72c2a89f2fb408f80bb511abab56f167eaeb8f
SagarMakhar <sagarmakhar@gmail.com>
2022-01-12 18:32:54 +0000

    SystemUI: Add support for black theme [1/2]
    Squashed:
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Apr 16 10:38:36 2022 +0530
    
        Fixes and improvements for Black Theme
    
        * Add to SettingsObserver for observing toggle behavior.
        * Apply only with dark theme - looks awful on light mode.
        * Add configuration listener to reapply theme when dark mode changes.
    
        Change-Id: Ib114fa5772c3db74b46b7656aeb0eabdf6d4de4d
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Wed Aug 24 21:19:57 2022 +0300
    
        Don't skip theme application if it's already current
    
        This fixes black theme and font/icon shape customizations.
    
        Change-Id: I8c3d3b4634e1ac60d28e4d5ef20bf9c2015e2e18
    
    Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
    Signed-off-by: rk134 <rahul@aospa.co>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Co-Authored-By: Michael Bestas <mkbestas@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -475,6 +475,31 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             // Force reload so that we update even when the main color has not changed
             reevaluateSystemTheme(true /* forceReload */);
         });
+        mSecureSettings.registerContentObserverForUserSync(
+                Settings.Secure.getUriFor(Settings.Secure.SYSTEM_BLACK_THEME),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        if (DEBUG) Log.d(TAG, "Overlay changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        if (mSkipSettingChange) {
+                            if (DEBUG) Log.d(TAG, "Skipping setting change");
+                            mSkipSettingChange = false;
+                            return;
+                        }
+                        reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
 
         // All wallpaper color and keyguard logic only applies when Monet is enabled.
         if (!mIsMonetEnabled) {
@@ -787,12 +812,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             mActivityManager.setThemeOverlayReady(currentUser);
         };
 
-        if (colorSchemeIsApplied(managedProfiles)) {
-            Log.d(TAG, "Skipping overlay creation. Theme was already: " + mColorScheme);
-            onCompleteCallback.run();
-            return;
-        }
-
         if (DEBUG) {
             Log.d(TAG, "Applying overlays: " + categoryToPackage.keySet().stream()
                     .map(key -> key + " -> " + categoryToPackage.get(key)).collect(
@@ -800,6 +819,13 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         }
 
         FabricatedOverlay[] fOverlays = null;
+
+        boolean nightMode = (mContext.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+        boolean isBlackTheme = mSecureSettings.getInt(Settings.Secure.SYSTEM_BLACK_THEME, 0) == 1
+                                && nightMode;
+
+        mThemeManager.setIsBlackTheme(isBlackTheme);
 
         if (mNeedsOverlayCreation) {
             mNeedsOverlayCreation = false;
@@ -811,6 +837,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mThemeManager.applyCurrentUserOverlays(categoryToPackage, fOverlays, currentUser,
                 managedProfiles, onCompleteCallback);
 
+        mThemeManager.applyBlackTheme(isBlackTheme);
     }
 
     private Style fetchThemeStyleFromSetting() {
