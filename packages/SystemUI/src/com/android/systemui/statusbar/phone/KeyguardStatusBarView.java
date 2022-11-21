f34c6e231164a7a1d7e1fbb2e76a05c400661658
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-22 01:55:32 +0530

    SystemUI: Add tuner option to show battery percentage only
    Bolt while charging inspired by a change from ezio84 <brabus84@gmail.com>:
    https://github.com/ezio84/abc_frameworks_base/commit/1f915e971f57f75cd71ad88411ca6e2037e1fcae
    
    Change-Id: Ib02ae13b337e9498aedbdc9818d56dd0ac466a80

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -255,6 +255,7 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 mMultiUserAvatar.setVisibility(View.GONE);
             }
         }
+        mBatteryView.setIsStatusBar(true);
         mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
     }
 
