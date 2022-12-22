f58fbec1a9e1f93682f27be7e2f6a2a4cbdb24b4
TheHardGamer <gamelovr695@gmail.com>
2022-12-22 10:18:11 +0530

    base: fixup 17db0bce5a52514863ba36eb4077c80f78abd94a
    Restore protologgroup since it breaks art's method verifier
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- libs/WindowManager/Shell/src/com/android/wm/shell/protolog/ShellProtoLogGroup.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/protolog/ShellProtoLogGroup.java
@@ -129,8 +129,8 @@ public enum ShellProtoLogGroup implements IProtoLogGroup {
         private static final String TAG_WM_SPLIT_SCREEN = "ShellSplitScreen";
         private static final String TAG_WM_DESKTOP_MODE = "ShellDesktopMode";
 
-        private static final boolean ENABLE_DEBUG = false;
-        private static final boolean ENABLE_LOG_TO_PROTO_DEBUG = false;
+        private static final boolean ENABLE_DEBUG = true;
+        private static final boolean ENABLE_LOG_TO_PROTO_DEBUG = true;
 
         private static final int START_ID = (int) (
                 UUID.nameUUIDFromBytes(ShellProtoLogGroup.class.getName().getBytes())
