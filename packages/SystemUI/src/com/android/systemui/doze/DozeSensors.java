ae937ed8799a2ae1acdaea89d29d6d0bdb3292ce
Michael Bestas <mkbestas@lineageos.org>
2023-06-15 15:06:41 +0300

    fixup! Partially revert "Assume sensors perform prox check"
    Change-Id: I2acde0e73f8040280b0cd42a899b0dda6a19abb4

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -554,13 +554,13 @@ public class DozeSensors {
                 boolean requiresTouchscreen
         ) {
             this(
-                sensor,
-                setting,
-                configured,
-                pulseReason,
-                reportsTouchCoordinates,
-                requiresTouchscreen,
-                true
+                    sensor,
+                    setting,
+                    configured,
+                    pulseReason,
+                    reportsTouchCoordinates,
+                    requiresTouchscreen,
+                    true
             );
         }
 
@@ -613,7 +613,8 @@ public class DozeSensors {
                     ignoresSetting,
                     requiresProx,
                     true,
-                    immediatelyReRegister
+                    immediatelyReRegister,
+                    requiresAod
             );
         }
 
@@ -628,7 +629,8 @@ public class DozeSensors {
                 boolean ignoresSetting,
                 boolean requiresProx,
                 boolean performsProxCheck,
-                boolean immediatelyReRegister
+                boolean immediatelyReRegister,
+                boolean requiresAod
         ) {
             this(
                     new Sensor[]{ sensor },
@@ -673,7 +675,8 @@ public class DozeSensors {
                     requiresProx,
                     true,
                     immediatelyReRegister,
-                    posture
+                    posture,
+                    requiresAod
             );
         }
 
@@ -689,7 +692,8 @@ public class DozeSensors {
                 boolean requiresProx,
                 boolean performsProxCheck,
                 boolean immediatelyReRegister,
-                @DevicePostureController.DevicePostureInt int posture
+                @DevicePostureController.DevicePostureInt int posture,
+                boolean requiresAod
         ) {
             mSensors = sensors;
             mSetting = setting;
