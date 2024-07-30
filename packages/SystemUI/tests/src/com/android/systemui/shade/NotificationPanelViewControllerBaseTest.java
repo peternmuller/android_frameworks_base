090342b052d1e9abf158465abec2b2e3cbd2aa57
Matt Pietal <mpietal@google.com>
2024-07-30 14:21:00 +0000

    Remove dumpmanager from KeyguardStatusView
    We just don't use it at all for any info, and it'll
    fix a leak.
    
    Fixes: 353503323
    Test: check heapdump
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:7daecd56bdb128f42efeb9841116004e7f140b08)
    Merged-In: I40049fb2e37d422a92cbf896eeaecc4622abc1d5
    Change-Id: I40049fb2e37d422a92cbf896eeaecc4622abc1d5

--- packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
@@ -481,7 +481,6 @@ public class NotificationPanelViewControllerBaseTest extends SysuiTestCase {
                 mKeyguardLogger,
                 mKosmos.getInteractionJankMonitor(),
                 mKeyguardInteractor,
-                mDumpManager,
                 mPowerInteractor));
 
         when(mAuthController.isUdfpsEnrolled(anyInt())).thenReturn(false);
