4657bbb6a5a44e6279ca1d31bb22644372f446bc
Vishnu Nair <vishnun@google.com>
2024-09-17 10:00:39 -0700

    Validate originating process for transferTouchGesture API
    Addresses a security vulnerability where a malicious process could
    potentially steal an active touch gesture from its host or embedded
    process. The fix ensures that the requested is the owner of the
    InputTransferToken. This adds an additional verification on top of
    the existing  association checks between the transferFrom and
    transferTo processes.
    
    Flag: EXEMPT security fix
    Bug: 364037868
    Test: presubmit
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4089c359361d8703bab3be0ab0a29723db76b356)
    Merged-In: I2654ccab807a62a341c8af69bf64bb33e56c4252
    Change-Id: I2654ccab807a62a341c8af69bf64bb33e56c4252

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -9248,6 +9248,8 @@ public class WindowManagerService extends IWindowManager.Stub
         final InputApplicationHandle applicationHandle;
         final String name;
         Objects.requireNonNull(outInputChannel);
+        Objects.requireNonNull(inputTransferToken);
+
         synchronized (mGlobalLock) {
             WindowState hostWindowState = hostInputTransferToken != null
                     ? mInputToWindowMap.get(hostInputTransferToken.getToken()) : null;
@@ -9272,6 +9274,7 @@ public class WindowManagerService extends IWindowManager.Stub
         Objects.requireNonNull(transferFromToken);
         Objects.requireNonNull(transferToToken);
 
+        final int callingUid = Binder.getCallingUid();
         final long identity = Binder.clearCallingIdentity();
         boolean didTransfer;
         try {
@@ -9281,12 +9284,14 @@ public class WindowManagerService extends IWindowManager.Stub
                 // represents an embedded window so transfer from host to embedded.
                 WindowState windowStateTo = mInputToWindowMap.get(transferToToken.getToken());
                 if (windowStateTo != null) {
-                    didTransfer = mEmbeddedWindowController.transferToHost(transferFromToken,
+                    didTransfer = mEmbeddedWindowController.transferToHost(callingUid,
+                            transferFromToken,
                             windowStateTo);
                 } else {
                     WindowState windowStateFrom = mInputToWindowMap.get(
                             transferFromToken.getToken());
-                    didTransfer = mEmbeddedWindowController.transferToEmbedded(windowStateFrom,
+                    didTransfer = mEmbeddedWindowController.transferToEmbedded(callingUid,
+                            windowStateFrom,
                             transferToToken);
                 }
             }
