bde24f5aa1e3ec1937a4837977c8966ca939ca6b
Shawn Lee <syeonlee@google.com>
2024-12-27 14:02:25 +0800

    [PATCH] Add carrier status bar icons to ignore list in DSDS
    Bug: 359870251
    Test: manually verified status bar icon visibility in dual sim and single sim in landscape/large screen and portrait
    Flag: EXEMPT bug fix
    Change-Id: Ia1ead7f4804e50f12e890f67bf23501bf712fc8e

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -202,6 +202,7 @@ constructor(
             if (qsVisible && field != value) {
                 header.alpha = ShadeInterpolation.getContentAlpha(value)
                 field = value
+                updateIgnoredSlots()
             }
         }
 
@@ -551,7 +552,7 @@ constructor(
 
     private fun updateIgnoredSlots() {
         // switching from QQS to QS state halfway through the transition
-        if (singleCarrier || qsExpandedFraction < 0.5) {
+        if (singleCarrier || (!largeScreenActive && qsExpandedFraction < 0.5)) {
             iconContainer.removeIgnoredSlots(carrierIconSlots)
         } else {
             iconContainer.addIgnoredSlots(carrierIconSlots)
