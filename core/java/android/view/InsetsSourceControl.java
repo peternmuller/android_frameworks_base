7ff592d20ee3c23de11842e88009f1802f636e42
zhangzhihao <zhangzhihao7@xiaomi.corp-partner.google.com>
2025-01-22 14:50:04 +0800

    Fix surfaceControl release exception
    Insets does not perform a check when releasing SurfaceControl. Now we perform a check when releasing it.
    
    Test: m
    Bug: 391478307
    
    Google: 3459833
    Change-Id: Id9b21be1c708d898c1fe0ba9a8a97ad09de3685a
    Signed-off-by: zhangzhihao <zhangzhihao7@xiaomi.corp-partner.google.com>

--- core/java/android/view/InsetsSourceControl.java
+++ core/java/android/view/InsetsSourceControl.java
@@ -173,7 +173,7 @@ public class InsetsSourceControl implements Parcelable {
     }
 
     public void release(Consumer<SurfaceControl> surfaceReleaseConsumer) {
-        if (mLeash != null) {
+        if (mLeash != null && mLeash.isValid()) {
             surfaceReleaseConsumer.accept(mLeash);
         }
     }
