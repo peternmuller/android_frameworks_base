4af7c4361002a950501d485cfaad642cf561786a
Richard Raya <rdxzv.dev@gmail.com>
2025-01-24 10:51:13 -0300

    SystemUI: Disable QS fling haptics
    Change-Id: I57e6a86196ed91a2c3f9cf76dab9f421a198385c
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -884,7 +884,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mNotificationsDragEnabled = mResources.getBoolean(
                 R.bool.config_enableNotificationShadeDrag);
         mVibratorHelper = vibratorHelper;
-        mVibrateOnOpening = mResources.getBoolean(R.bool.config_vibrateOnIconAnimation);
+        mVibrateOnOpening = false;
         mStatusBarTouchableRegionManager = statusBarTouchableRegionManager;
         mSystemClock = systemClock;
         mKeyguardMediaController = keyguardMediaController;
